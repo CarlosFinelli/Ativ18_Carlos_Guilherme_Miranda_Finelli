72e84729146e2fe81914b0fa55e8ace9aaed9a71
Carlos Guilherme Miranda Finelli <60704748+CarlosFinelli@users.noreply.github.com>
2020-11-24 13:46:34 -0300

    Conclusão da atividade

---
+++ Ativ18/Ativ18/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Ativ18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int? A = null;
+            long? b = null;
+            ushort? c = null;
+            double? d = null;
+            int e = A ?? 34;
+            long f = b ?? 65000000;
+            ushort g = c ?? 500;
+            double h = d ?? 7.50;
+            int i = A.GetValueOrDefault(e);
+            Console.WriteLine(i);
+        }
+    }
+}
